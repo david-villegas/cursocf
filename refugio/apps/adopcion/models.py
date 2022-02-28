de81225d56e8300ab05a94bd0a4d32213e835ff1
David Villegas <david.villegasg7@gmail.com>
2022-02-28 11:18:27 -0400

    Initial Commit

---
+++ refugio/apps/adopcion/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Persona(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    edad = models.IntegerField()
+    telefono = models.CharField(max_length=12)
+    email = models.EmailField()
+    direccion = models.TextField()
