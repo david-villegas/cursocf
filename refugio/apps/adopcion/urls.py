fb22a7b2f6731ea50cef64abfa4cd90f43b002fa
David Villegas <david.villegasg7@gmail.com>
2022-03-05 22:04:01 -0400

    update & delete views

--- refugio/apps/adopcion/urls.py
+++ refugio/apps/adopcion/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from apps.adopcion.views import index
+# from apps.adopcion.views import index
+from . import views
 
 app_name = 'adopcion'
+# urlpatterns = [
+#     path('', index),
+# ]
+
 urlpatterns = [
-    path('', index),
+    path('', views.index, name='index'),
 ]
--- refugio/apps/mascota/urls.py
+++ refugio/apps/mascota/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 
-from apps.mascota.views import index,mascota_view, mascota_list
+# from apps.mascota.views import index,mascota_view, mascota_list
+from . import views
 
 app_name = 'mascota'
+# urlpatterns = [
+#     path('', index, name='index'),
+#     path('nuevo', mascota_view, name='mascota_crear'),
+#     path('listar', mascota_list, name='mascota_listar'),
+# ]
+
 urlpatterns = [
-    path('', index, name='index'),
-    path('nuevo', mascota_view, name='mascota_crear'),
-    path('listar', mascota_list, name='mascota_listar'),
+    path('', views.index, name='index'),
+    path('nuevo', views.mascota_view, name='mascota_crear'),
+    path('listar', views.mascota_list, name='mascota_listar'),
+    path('editar/<id_mascota>', views.mascota_edit, name='mascota_editar'),
+    path('eliminar/<id_mascota>', views.mascota_delete, name='mascota_eliminar'),
 ]
