1dfc3e3a959a488b2bf07c2b87e0faad0eabc0e7
David Villegas <david.villegasg7@gmail.com>
2022-03-09 21:11:20 -0400

    App Adopcion UpdateView & DeleteView

--- refugio/apps/adopcion/urls.py
+++ refugio/apps/adopcion/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('solicitud/listar',views.SolicitudList.as_view(), name='solicitud_listar'),
     path('solicitud/nueva',views.SolicitudCreate.as_view(), name='solicitud_crear'),
+    path('solicitud/editar/<pk>',views.SolicitudUpdate.as_view(), name='solicitud_editar'),
+    path('solicitud/eliminar/<pk>',views.SolicitudDelete.as_view(), name='solicitud_eliminar'),
 ]
--- refugio/apps/adopcion/views.py
+++ refugio/apps/adopcion/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import ListView,CreateView,UpdateView,DeleteView
 from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
 
 from apps.adopcion.models import Persona, Solicitud
 from apps.adopcion.forms import PersonaForm, SolicitudForm
@@ -15,8 +16,9 @@ def index(request):
 class SolicitudList(ListView):
     model = Solicitud
     template_name = 'adopcion/solicitud_list.html'
+    ordering = ['id']
 
-class SolicitudCreate(CreateView):
+class SolicitudCreate(SuccessMessageMixin, CreateView):
     model = Solicitud
     template_name = 'adopcion/solicitud_form.html'
     form_class = SolicitudForm
@@ -43,3 +45,44 @@ class SolicitudCreate(CreateView):
             return HttpResponseRedirect(self.get_success_url())
         else:
             return self.render_to_response(self.get_context_data(form=form, form2=form2))
+
+class SolicitudUpdate(SuccessMessageMixin, UpdateView):
+    model = Solicitud
+    second_model = Persona
+    template_name = 'adopcion/solicitud_form.html'
+    form_class = SolicitudForm
+    second_form_class = PersonaForm
+    success_url = reverse_lazy('adopcion:solicitud_listar')
+    success_message = 'Registro actualizado Satisfactoriamente.'
+
+    def get_context_data(self, **kwargs):
+        context = super(SolicitudUpdate, self).get_context_data(**kwargs)
+        pk = self.kwargs.get('pk', 0)
+        solicitud = self.model.objects.get(id=pk)
+        persona = self.second_model.objects.get(id=solicitud.persona_id)
+        if 'form' not in context:
+            context['form'] = self.form_class()
+        if 'form2' not in context:
+            context['form2'] = self.second_form_class(instance=persona)
+        context['id'] = pk
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object
+        id_solicitud = kwargs['pk']
+        solicitud = self.model.objects.get(id= id_solicitud)
+        persona = self.second_model.objects.get(id=solicitud.persona_id)
+        form = self.form_class(request.POST, instance=solicitud)
+        form2 = self.second_form_class(request.POST, instance=persona)
+        if form.is_valid() and form2.is_valid():
+            form.save()
+            form2.save()
+            return HttpResponseRedirect(self.get_success_url())
+        else:
+            return HttpResponseRedirect(self.get_success_url())
+
+class SolicitudDelete(SuccessMessageMixin, DeleteView):
+    model = Solicitud
+    template_name = 'adopcion/solicitud_delete.html'
+    success_url = reverse_lazy('adopcion:solicitud_listar')
+    success_message = 'Registro eliminado Satisfactoriamente.'
