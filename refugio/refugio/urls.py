36b290923e3ae75e6f451d22c4552703fd3c790b
David Villegas <david.villegasg7@gmail.com>
2022-03-20 21:25:19 -0400

    Password Reset con envio de email

--- refugio/refugio/urls.py
+++ refugio/refugio/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +24,8 @@ urlpatterns = [
     path('adopcion/', include('apps.adopcion.urls')),
     path('usuario/', include('apps.usuario.urls')),
     path('', LoginView.as_view(template_name='usuario/index.html'), name='login'),
+    path('reset/password_reset', PasswordResetView.as_view(template_name='registration/password_reset_form.html', email_template_name='registration/password_reset_email.html'), name='password_reset'),
+    path('reset/password_reset_done', PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done', PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 ]
